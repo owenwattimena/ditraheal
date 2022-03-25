dafeef14481ea03b63f8d750ac68ff2089c76d11
FlutLab.io <>
2022-03-25 05:00:11 +0000

    Autosave

--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -4,31 +4,31 @@ import '../models/api_return_value.dart';
 
 class Provider {
   static Future<ApiReturnValue> request(Function doSomething) async {
+    ApiReturnValue result;
     try {
-      return doSomething as ApiReturnValue;
+      result = doSomething as ApiReturnValue;
     } on TimeoutException {
       // print("time out");
-      return ApiReturnValue(
+      result = ApiReturnValue(
         data: null,
         message: "error. Request timeout",
         statusCode: 408,
       );
     } on SocketException {
       // print("socket");
-
-      return ApiReturnValue(
+      result = ApiReturnValue(
         data: null,
         message: "error. SocketException",
         statusCode: 400,
       );
     } catch (e) {
       // print("catch : $e");
-
-      return ApiReturnValue(
+      result = ApiReturnValue(
         data: null,
         message: "error. ${e.toString()}",
         statusCode: 400,
       );
     }
+    return result;
   }
 }
