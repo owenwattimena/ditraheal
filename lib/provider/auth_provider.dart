cb01d756cec5e03384903ae4aa17412257e33287
FlutLab.io <>
2022-03-25 05:56:38 +0000

    Autosave

--- lib/provider/auth_provider.dart
+++ lib/provider/auth_provider.dart
@@ -5,53 +5,19 @@ import '../models/api_return_value.dart';
 import 'provider.dart';
 import '../utils/constants.dart';
 // import 'package:get/get.dart';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+// import 'dart:convert';
+// import 'package:http/http.dart' as http;
 
 class AuthProvider {
   /// Fetch API TOKEN data from API
   Future<ApiReturnValue> fetchToken({String username = "adm", String password = "jok742n"}) async {
-    print("fetch token");
-    return Provider.request(() async {
-      var url = Uri.parse(LOGIN_URL);
-      Map<String, String> body = {
-        'username': username,
-        'password': password,
-      };
-      http.Client client = http.Client();
-      var response = await client
-          .post(
-            url,
-            headers: {
-              "Content-Type": "application/json; charset=UTF-8",
-              "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Credentials": "true", // Required for cookies, authorization headers with HTTPS
-              "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
-              "Access-Control-Allow-Methods": "POST, OPTIONS"
-            },
-            body: json.encode(body),
-          )
-          .timeout(Duration(seconds: 10));
-      var jsonResponse = json.decode(response.body);
-      print(jsonResponse);
-      if (response.statusCode == 200) {
-        // print("success");
-        return ApiReturnValue(
-          data: jsonResponse,
-          message: "success",
-          statusCode: 200,
-        );
-      } else {
-        // print("error");
-        return ApiReturnValue(
-          data: jsonResponse,
-          message: "error",
-          statusCode: response.statusCode,
-        );
-      }
-    });
-
-    // try {
+    Map body = {
+      'username': username,
+      'password': password
+    };
+    return Provider.requestPost(LOGIN_URL, body);
+    // print("fetch token");
+    // return Provider.request(() async {
     //   var url = Uri.parse(LOGIN_URL);
     //   Map<String, String> body = {
     //     'username': username,
@@ -72,7 +38,9 @@ class AuthProvider {
     //       )
     //       .timeout(Duration(seconds: 10));
     //   var jsonResponse = json.decode(response.body);
+    //   print(jsonResponse);
     //   if (response.statusCode == 200) {
+    //     // print("success");
     //     return ApiReturnValue(
     //       data: jsonResponse,
     //       message: "success",
@@ -85,25 +53,7 @@ class AuthProvider {
     //       statusCode: response.statusCode,
     //     );
     //   }
-    // } on TimeoutException {
-    //   return ApiReturnValue(
-    //     data: null,
-    //     message: "error. Request timeout",
-    //     statusCode: 408,
-    //   );
-    // } on SocketException {
-    //   return ApiReturnValue(
-    //     data: null,
-    //     message: "error. SocketException",
-    //     statusCode: 400,
-    //   );
-    // } catch (e) {
-    //   return ApiReturnValue(
-    //     data: null,
-    //     message: "error. ${e.toString()}",
-    //     statusCode: 400,
-    //   );
-    // }
+    // });
   }
 
   // Future<ApiReturnValue> doSignup({String? token}) async {
--- lib/provider/provider.dart
+++ lib/provider/provider.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:io';
 import '../models/api_return_value.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Provider {
   static Future<ApiReturnValue> request(Future<ApiReturnValue> Function() doSomething) async {
@@ -22,7 +24,6 @@ class Provider {
         statusCode: 400,
       );
     } catch (e) {
-      print("catch : $e");
       result = ApiReturnValue(
         data: null,
         message: "error. ${e.toString()}",
@@ -30,5 +31,41 @@ class Provider {
       );
     }
     return result;
+  }
+
+  static Future<ApiReturnValue> requestPost(String url, Map body) async {
+    return Provider.request(() async {
+      var uri = Uri.parse(url);
+      http.Client client = http.Client();
+      var response = await client
+          .post(
+            uri,
+            headers: {
+              "Content-Type": "application/json; charset=UTF-8",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Credentials": "true", // Required for cookies, authorization headers with HTTPS
+              "Access-Control-Allow-Headers": "Origin,Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,locale",
+              "Access-Control-Allow-Methods": "POST, OPTIONS"
+            },
+            body: json.encode(body),
+          )
+          .timeout(Duration(seconds: 10));
+      var jsonResponse = json.decode(response.body);
+      print(jsonResponse);
+      if (response.statusCode == 200) {
+        // print("success");
+        return ApiReturnValue(
+          data: jsonResponse,
+          message: "success",
+          statusCode: 200,
+        );
+      } else {
+        return ApiReturnValue(
+          data: jsonResponse,
+          message: "error",
+          statusCode: response.statusCode,
+        );
+      }
+    });
   }
 }
